5aba0a17873747d3e2d629608ace4eff56dad229
mwamba <mwambajoakim@gmail.com>
2025-07-28 17:36:17 +0300

    Test __str__ method

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -11,6 +11,9 @@ class TestBaseModel(unittest.TestCase):
         self.my_model = BaseModel()
         self.my_model.name = "First Model"
         self.my_model.number = 100
+        created_at = self.my_model.created_at
+        updated_at = self.my_model.updated_at
+        old_save = self.my_model.save()
 
     def test_name(self):
         """Tests the name of instance"""
@@ -24,3 +27,9 @@ class TestBaseModel(unittest.TestCase):
         """Tests the __str__ method"""
         expected_str = f"[BaseModel] ({self.my_model.id}) {self.my_model.__dict__}"
         self.assertEqual(str(self.my_model), expected_str)
+
+    def test_save(self):
+        """Tests the save method in BaseModel"""
+        new_save = self.my_model.save()
+        self.assertNotEqual(self.old_save, self.new_save)
+        
