b5603cfb9507e64e9fe0791871fe035b0d0be433
mwamba <mwambajoakim@gmail.com>
2025-08-02 21:57:00 +0300

    Import needed modules

---
+++ tests/test_file_storage.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""All tests for class FileStorage are herein"""
+from models.base_model import BaseModel
+import os
+from models.engine.file_storage import FileStorage
+import unittest
+
