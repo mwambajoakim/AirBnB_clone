0acde7e1d2ab2045a05f3929f85f8926ac712db5
mwamba <mwambajoakim@gmail.com>
2025-08-07 18:55:37 +0300

    Define "show": To show a particular instance

--- console.py
+++ console.py
@@ -5,10 +5,13 @@
 import cmd
 import sys
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb)'
+    file = storage.reload()
 
     def do_EOF(self, line):
         """Checks end of file. If true, exits gracefully.
@@ -33,8 +36,29 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
         else:
             arg = BaseModel()
+            arg.save()
             print(arg.id)
-    
+
+    def do_show(self, arg):
+        """Prints string representation of instance
+           based on the name and id
+        """
+        args = arg.split()
+
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** class id missing **")
+        else:
+            key = f"{args[0]}.{args[1]}"
+            models = storage.all()
+            if key in models:
+                print(models[key])
+            else:
+                print("** instance doesn't exist **")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
