92c96c7a5fefc3ba17efd25c38d38c4f4cd7a81b
mwamba <mwambajoakim@gmail.com>
2025-08-09 17:35:01 +0300

    Define public attributes

--- models/review.py
+++ models/review.py
@@ -3,3 +3,13 @@
 
 
 class Review(BaseModel):
+    """Creates an instance of Review class.
+
+       Args:
+            place_id: ID of the place(Will be be Place.id).
+            user_id: ID of the user(Will be User.id).
+            text: The review from the user.
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
