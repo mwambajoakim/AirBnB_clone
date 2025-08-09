b30f6ae7e71cc8dff5294a62bd1a8e0402377e15
mwamba <mwambajoakim@gmail.com>
2025-08-09 16:33:46 +0300

    Import appropriate classes

--- models/__init__.py
+++ models/__init__.py
@@ -5,6 +5,8 @@ Creates a variable 'storage'
 Calls reload() on 'storage' variable
 """
 from models.engine.file_storage import FileStorage
+from models.user import User
+from models.base_model import BaseModel
 
 
 storage = FileStorage()
