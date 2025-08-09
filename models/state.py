6cbdc24aafe5f279a967b1e136bb7806d7e3f4ee
mwamba <mwambajoakim@gmail.com>
2025-08-09 17:13:53 +0300

    Define public attribute name

--- models/state.py
+++ models/state.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-"""Creates the state of the user"""
+"""Initializes the state of the user"""
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
+    """Creates an instance of the state.
+
+       Args:
+            name: Name of the state -> public attribute
+    """
+    name = ""
