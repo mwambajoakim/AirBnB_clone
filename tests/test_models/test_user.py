012f00a048d13787bd87205397c1ec84160b6604
mwamba <mwambajoakim@gmail.com>
2025-08-11 16:42:22 +0300

    Test to_dict

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Test the class User"""
+from models.user import User
+import unittest
+import datetime
+
+
+class TestUser(unittest.TestCase):
+    def setUp(self):
+        self.my_user = User()
+        self.my_user.first_name = "Joe"
+        self.my_user.last_name = "Log"
+        self.my_user.email = "joelog@email.com"
+        self.my_user.password = "jl1234"
+        created_at = self.my_user.created_at
+        updated_at = self.my_user.updated_at
+
+    def test_user_fname(self):
+        self.assertEqual(self.my_user.first_name, "Joe")
+
+    def test_user_lname(self):
+        self.assertEqual(self.my_user.last_name, "Log")
+
+    def test_user_email(self):
+        self.assertEqual(self.my_user.email, "joelog@email.com")
+
+    def test_user_pass(self):
+        self.assertEqual(self.my_user.password, "jl1234")
+
+    def test_created_at(self):
+        self.assertTrue(hasattr(self.my_user, "created_at"))
+        self.assertTrue(isinstance(self.my_user, User))
+        #self.assertTrue(issubclass(self.my_user, BaseModel))
+
+    def test_updated_at(self):
+        self.assertTrue(hasattr(self.my_user, "updated_at"))
+
+    def test_to_dict(self):
+        my_dict = self.my_user.to_dict()
+        self.assertEqual(my_dict["id"], self.my_user.id)
+        self.assertEqual(my_dict["created_at"], self.my_user.created_at.isoformat())
+        self.assertEqual(my_dict["updated_at"], self.my_user.updated_at.isoformat())
+        
