097dfb69b836d58dd06c931de932befd0fbb4e69
mwamba <mwambajoakim@gmail.com>
2025-08-12 16:53:33 +0300

    Test has attribute updated_at

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -20,3 +20,14 @@ class TestCity(unittest.TestCase):
     def test_created_at(self):
         """Tests the instance has an attribute 'created_at'"""
         self.assertTrue(hasattr(self.my_city, "created_at"))
+
+    def test_updated_at(self):
+        """Tests the instance has an attribute 'updated_at'"""
+        self.assertTrue(hasattr(self.my_city, "updated_at"))
+
+        old_update = self.my_city.updated_at
+        time.sleep(0.50)
+        self.my_city.save()
+        new_update = self.my_city.updated_at
+        self.assertNotEqual(old_update, new_update)
+        self.assertLess(old_update, new_update)
