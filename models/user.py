5a77bbe67eeb64f4e0f2fc8cd5cf3aaa56259b93
mwamba <mwambajoakim@gmail.com>
2025-08-13 20:39:59 +0300

    Write documentation

--- models/user.py
+++ models/user.py
@@ -4,6 +4,14 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
+    """Instantiates a user
+
+       Args:
+            first_name: First name of the user.
+            last_name: Last name of the user.
+            email: Email of the user.
+            password: Password of the user.
+    """
     first_name = ""
     last_name = ""
     email = ""
